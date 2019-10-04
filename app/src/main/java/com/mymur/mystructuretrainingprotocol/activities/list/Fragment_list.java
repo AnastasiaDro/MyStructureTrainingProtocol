a2d440b14a94bdea42ce285659ad1f07aece8e79
AnastasiaDro <adrogunova@gmail.com>
2019-10-04 23:45:45 +0300

    -changed Fragment_list from extends fragment to extends FragmentList

--- app/src/main/java/com/mymur/mystructuretrainingprotocol/activities/list/Fragment_list.java
+++ app/src/main/java/com/mymur/mystructuretrainingprotocol/activities/list/Fragment_list.java
@@ -5,18 +5,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.mymur.mystructuretrainingprotocol.R;
 
-public class Fragment_list extends Fragment implements View.OnClickListener {
+import java.util.ArrayList;
+
+public class Fragment_list extends ListFragment implements View.OnClickListener {
 
     Integer position;
     TypedArray dataArray;
+    ArrayList dataList;
     Button addNew;
     View view;
 
@@ -28,29 +34,36 @@ public class Fragment_list extends Fragment implements View.OnClickListener {
 
 
    //делаем фрагмент
-   @Override
-   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-       dataArray = getResources().obtainTypedArray(R.array.students);
-       position = getArguments().getInt("CurrentStudent");
+//   @Override
+//   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+//       dataArray = getResources().obtainTypedArray(R.array.students);
+//       position = getArguments().getInt("CurrentStudent");
+//
+//    //   view = inflater.inflate(R.layout.list_fragment, container, false);
+//       //настраиваем recyclerView
+////       recyclerView = (RecyclerView)view.findViewById(R.id.recyclerView);
+////       recyclerView.setHasFixedSize(true);
+////       layoutManager = new LinearLayoutManager(getActivity());
+////       recyclerView.setLayoutManager(layoutManager);
+////
+////       mAdapter = new MyAdapter(dataArray);
+////       recyclerView.setAdapter(mAdapter);
+////
+////       return view;
+//
+//   }
 
-       view = inflater.inflate(R.layout.list_fragment, container, false);
-       //настраиваем recyclerView
-       recyclerView = (RecyclerView)view.findViewById(R.id.recyclerView);
-       recyclerView.setHasFixedSize(true);
-       layoutManager = new LinearLayoutManager(getActivity());
-       recyclerView.setLayoutManager(layoutManager);
-
-       mAdapter = new MyAdapter(dataArray);
-       recyclerView.setAdapter(mAdapter);
-
-
-
-
-
-       return view;
-   }
-
-
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        dataArray = getResources().obtainTypedArray(R.array.students);
+        dataList = new ArrayList();
+        for (int i = 0; i < dataArray.length(); i++) {
+            dataList.add(dataArray.getString(i));
+        }
+        super.onActivityCreated(savedInstanceState);
+        ListAdapter adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, dataList);
+        setListAdapter(adapter);
+    }
 
 
     @Override
