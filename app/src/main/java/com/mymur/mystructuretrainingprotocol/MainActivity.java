146846916a8ee26b8b6d1897c469d9a075ba4024
AnastasiaDro <adrogunova@gmail.com>
2019-10-04 07:04:54 +0300

    First commit

---
+++ app/src/main/java/com/mymur/mystructuretrainingprotocol/MainActivity.java
@@ -0,0 +1,17 @@
+package com.mymur.mystructuretrainingprotocol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+//здесь будет поле входа
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
