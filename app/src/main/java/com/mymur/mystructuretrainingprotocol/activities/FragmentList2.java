76518cc9b016fa672a7eca94680cc57a76cdee6c
AnastasiaDro <adrogunova@gmail.com>
2019-10-04 23:58:03 +0300

    did something... new error

--- app/src/main/java/com/mymur/mystructuretrainingprotocol/activities/FragmentList2.java
+++ app/src/main/java/com/mymur/mystructuretrainingprotocol/activities/FragmentList2.java
@@ -1,22 +1,49 @@
 package com.mymur.mystructuretrainingprotocol.activities;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.mymur.mystructuretrainingprotocol.R;
+import com.mymur.mystructuretrainingprotocol.activities.list.MyAdapter;
+
+import java.util.ArrayList;
 
 public  class FragmentList2 extends Fragment {
     View view;
+    Integer position;
+    TypedArray dataArray;
+    ArrayList dataList;
+    Button addNew;
+    
 
+
+    //графика с recyclerView
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.list_fragment, container, false);
 
-        return view;
+        recyclerView = (RecyclerView)view.findViewById(R.id.recyclerView);
+       recyclerView.setHasFixedSize(true);
+       layoutManager = new LinearLayoutManager(getActivity());
+       recyclerView.setLayoutManager(layoutManager);
+
+       mAdapter = new MyAdapter(dataArray);
+       recyclerView.setAdapter(mAdapter);
+
+       return view;
+
+
     }
 }
